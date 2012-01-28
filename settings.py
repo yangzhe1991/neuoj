a88d1af9ff807ed2756f1b77925fbf7f5310effe
Philo Yang <ud1937@gmail.com>
2012-01-29 01:50:15 +0800

    new start

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ DATABASES = {
 			'ENGINE': 'mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 			'NAME': 'neuoj',                      # Or path to database file if using sqlite3.
 			'USER': 'root',                      # Not used with sqlite3.
-			'PASSWORD': 'yangzhe1991',                  # Not used with sqlite3.
+			'PASSWORD': '',                  # Not used with sqlite3.
 			'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
 			'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
 			}
@@ -69,7 +69,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-		'/home/godfather/code/neuoj/template/static',
+		'/home/godfather/git/neuoj/template/static',
 		# Put strings here, like "/home/html/static" or "C:/www/django/static".
 		# Always use forward slashes, even on Windows.
 		# Don't forget to use absolute paths, not relative paths.
@@ -108,7 +108,7 @@ TEMPLATE_DIRS = (
 		# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
 		# Always use forward slashes, even on Windows.
 		# Don't forget to use absolute paths, not relative paths.
-		'/home/godfather/code/neuoj/template',
+		'/home/godfather/git/neuoj/template',
 		)
 
 INSTALLED_APPS = (
